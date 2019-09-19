2a3a3b650ecaaf6243055d7ce0d99b890254d272
SpQuyt <spquyt@gmail.com>
2019-09-19 22:30:09 +0700

    Remove /easy-event from APIs url

--- src/configs/server.js
+++ src/configs/server.js
@@ -1,4 +1,4 @@
 // const Server = "http://127.0.0.1:3005"
-const Server = "https://api.easy-event-v1.namdaoduy.dev/easy-event"
+const Server = "https://api.easy-event-v1.namdaoduy.dev"
 
 export { Server }
